99b86ea210bebb0c6cfede3b6f157f7f78a4ea11
Mihkel Meier <mihkel.meier@tktk.ee>
2023-05-04 21:35:10 +0300

    add getmovie route

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { default: axios } = require("axios");
 const app = express();
+const http = require("http");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 app.get("/", (req, res) =>{
     let url = "https://api.themoviedb.org/3/movie/118340?api_key=95fce9d1198043b454f93509d77107b4";
@@ -80,6 +82,52 @@ app.get("/", (req, res) =>{
         });
         
 });
+
+app.post('/getmovie', (req, res) => {
+	const movieToSearch = 
+		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+			? req.body.queryResult.parameters.movie
+			: '';
+
+	const reqUrl = encodeURI(
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=997c6252`
+	);
+	http.get(
+		reqUrl,
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				const movie = JSON.parse(completeResponse);
+                if (!movie || !movie.Title) {
+                    return res.json({
+                        fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
+                        source: 'getmovie'
+                    });
+                }
+
+				let dataToSend = movieToSearch;
+				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+					movie.Director
+				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
+
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'getmovie'
+				});
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'getmovie'
+			});
+		}
+	)
+});
+
             app.listen(process.env.PORT || 3000, ()=> {
             console.log("server is running.");
             });    
